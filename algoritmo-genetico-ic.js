0e26ea75d4a5af2a8707d9c92e7d4a9a394fa69c
gpgaliperez <gp.galiperez@gmail.com>
2021-11-09 00:28:59 -0300

    Fixed

--- algoritmo-genetico-ic.js
+++ algoritmo-genetico-ic.js
@@ -9,7 +9,7 @@ const demes = false; //para que los cromosomas en el torneo sean consecutivos
 /**
  * Generate a population of Genotypes of length 'size'
  * @param {number} size
- * 
+ *
  */
 generatePopulation = (size) => {
   const population = [];
@@ -43,18 +43,17 @@ fitness = (cromosoma) => {
 tournament = (G1, G2) => {
   const f1 = fitness(G1);
   const f2 = fitness(G2);
-  console.log("---SELECCIÓN POR TORNEO---")
-  console.log("Competidores: " + G1 + "(" + f1 + "), "+ G2 + "(" + f2 + ")")
+  console.log('---SELECCIÓN POR TORNEO---');
+  console.log('Competidores: ' + G1 + '(' + f1 + '), ' + G2 + '(' + f2 + ')');
   if (f1 > f2) {
-    console.log("Cromosoma ganador: G1")
-    console.log("------")
+    console.log('Cromosoma ganador: G1');
+    console.log('------');
     return [1, 0];
   } else {
-    console.log("Cromosoma ganador G2")
-    console.log("------")
+    console.log('Cromosoma ganador G2');
+    console.log('------');
     return [0, 1];
   }
-
 };
 
 //CRUZA EN 2 PUNTOS
@@ -65,8 +64,9 @@ tournament = (G1, G2) => {
  */
 twoPointCrossover = (padre1, padre2) => {
   let son1, son2, sections;
-  console.log("---CRUZA EN 2 PUNTOS---")
-  console.log("Competidores: " + padre1 + ", " + padre2)
+  console.log('---CRUZA EN 2 PUNTOS---');
+  console.log('Competidores: ' + padre1 + ', ' + padre2);
+  let cut = 0;
   do {
     sections = [
       Math.floor(Math.random() * padre1.length),
@@ -82,10 +82,12 @@ twoPointCrossover = (padre1, padre2) => {
       padre1.slice().splice(sections[0], sections[1] - sections[0]),
       padre2.slice().splice(sections[1])
     );
-  } while (fitness(padre1) > 700 || fitness(padre2) > 700);
-  console.log("Hijos resultantes: " + son1 + ", " + son2);
-  console.log("------")
-  return [son1, son2];
+    cut += 1;
+  } while ((fitness(son1) > 700 || fitness(son2) > 700) && cut < 15);
+  console.log('Hijos resultantes: ' + son1 + ', ' + son2);
+  console.log('------');
+
+  return cut >= 15 ? [padre1, padre2] : [son1, son2];
 };
 
 //MUTACIÓN - REVERTIR
@@ -94,41 +96,41 @@ twoPointCrossover = (padre1, padre2) => {
  * @param {Genotype} G
  */
 mutate = (G) => {
-  if(Math.random() < probability) {
+  if (Math.random() < probability) {
     do {
       const pos = Math.floor(Math.random() * G.length);
       G[pos] = G[pos] ? 0 : 1;
     } while (fitness(G) > 700);
   }
   return G;
-}
+};
 
 // AUXILIARES
 
-  /*
-   * Select two random indices of the population
-   */
+/*
+ * Select two random indices of the population
+ */
 selectRandomGenotypes = (tamanioPadres) => {
-    let index1,index2;
-    while(index1 === index2) {
-        index1 = Math.floor(Math.random()*tamanioPadres);
-        index2 = Math.floor(Math.random()*tamanioPadres);
-    }
-    return [index1,index2];
-}
+  let index1, index2;
+  while (index1 === index2) {
+    index1 = Math.floor(Math.random() * tamanioPadres);
+    index2 = Math.floor(Math.random() * tamanioPadres);
+  }
+  return [index1, index2];
+};
 
 mejorFitness = (poblacion) => {
-  let cromosoma
-  const resultado = poblacion.reduce( (max, c) => {
-    if(fitness(c) > max && fitness(c) > minFitness) {
-      cromosoma = [...c]
-      return fitness(c)
+  let cromosoma;
+  const resultado = poblacion.reduce((max, c) => {
+    if (fitness(c) > max && fitness(c) > minFitness) {
+      cromosoma = [...c];
+      return fitness(c);
     }
-    return max
-  }, 0)
-  
-  return {cromosoma, resultado}
-}
+    return max;
+  }, 0);
+
+  return { cromosoma, resultado };
+};
 
 // -- INICIO del programa
 generatePopulation(10);
@@ -136,16 +138,15 @@ const maxNumeroDeGeneraciones = 10;
 let nroGeneracion = 0;
 
 do {
-
   let index = 0;
   let padres = [];
   let hijos = [];
-  
+
   // Seleccionar padres a cruzar
   for (let i = 0; i < poblacion.length / 2; i++) {
-    const resultadoTorneo = tournament(poblacion[index], poblacion[index+1]);
-    padres.push(resultadoTorneo[0] ? poblacion[index] : poblacion[index+1]);
-    index+=1;
+    const resultadoTorneo = tournament(poblacion[index], poblacion[index + 1]);
+    padres.push(resultadoTorneo[0] ? poblacion[index] : poblacion[index + 1]);
+    index += 2;
   }
   do {
     const padresACruzar = selectRandomGenotypes(padres.length);
@@ -153,20 +154,21 @@ do {
       padres[padresACruzar[0]],
       padres[padresACruzar[1]]
     );
-  
+
     hijos.push(mutate(cruza[0]));
     hijos.push(mutate(cruza[1]));
-  
-  } while(hijos.length < poblacion.length)
-  
+  } while (hijos.length < poblacion.length);
+
   poblacion = [...hijos];
-  nroGeneracion+=1;
-  console.log("---GENERACIÓN NÚMERO: " + nroGeneracion + "---" )
-  
-} while(nroGeneracion < maxNumeroDeGeneraciones || !mejorFitness(poblacion).resultado)
+  nroGeneracion += 1;
+  console.log('---GENERACIÓN NÚMERO: ' + nroGeneracion + '---');
+} while (
+  nroGeneracion < maxNumeroDeGeneraciones ||
+  !mejorFitness(poblacion).resultado
+);
 
 // Resultado Final
-const {cromosoma, resultado} = mejorFitness(poblacion)
-console.log("POBLACIÓN FINAL: " + poblacion + " NUMERO: " + nroGeneracion);
-console.log("CROMOSOMA GANADOR: " + cromosoma + ",  FITNESS: " + resultado);
-
+const { cromosoma, resultado } = mejorFitness(poblacion);
+console.log('POBLACIÓN FINAL: ', poblacion)
+console.log('GENERACIÓN NUMERO: ' + nroGeneracion);
+console.log('CROMOSOMA GANADOR: ' + cromosoma + ',  FITNESS: ' + resultado);
