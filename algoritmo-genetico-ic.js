4f118640daf31a312d7a6d202b7112e540065867
gpgaliperez <gp.galiperez@gmail.com>
2021-11-13 12:22:40 -0300

    Agrega comentarios

--- algoritmo-genetico-ic.js
+++ algoritmo-genetico-ic.js
@@ -1,42 +1,57 @@
 const probability = 0.5;
-const minFitness = 680;
+const minFitness = 690;
 const geneLength = 10;
 const genes = [0, 1];
 let poblacion = [];
 const pesos = [100, 155, 50, 112, 70, 80, 60, 118, 110, 55];
-const demes = false; //para que los cromosomas en el torneo sean consecutivos
 
 /**
- * Generate a population of Genotypes of length 'size'
+ * Generar población inicial aleatoriamente, utilizando
+ * "size" para determinar la cantidad de cromosomas que
+ * compondrán el mismo
  * @param {number} size
- *
  */
 generatePopulation = (size) => {
   const population = [];
   let i = 0;
   do {
     const genotype = [];
+    /**
+     * Iteramos según el largo del cromosoma (geneLength) y le asignamos
+     * uno de los valores que puede tomar esa posición, en este caso 0 o 1
+     */
     for (let i = 0; i < geneLength; i++) {
       const randomGene = genes[Math.floor(Math.random() * genes.length)];
       genotype.push(randomGene);
     }
+    /**
+     * Evaluamos el fitness y de superar 700 no se agrega ese cromosoma
+     * a la población
+     */
     if (fitness(genotype) <= 700) {
       population.push(genotype);
     }
     i++;
   } while (population.length < size);
+  /**
+   * Una vez llegado al número de cromosomas deseado se setea la población
+   */
   poblacion = [...population];
 };
 
+/**
+ * Función de fitness: se suman los pesos multiplicados por el valor de los alelos
+ */
 fitness = (cromosoma) => {
   return cromosoma.reduce((acc, bit, index) => (acc += bit * pesos[index]), 0);
 };
 
-//SELECCIÓN POR TORNEO
 /**
- * Compare fitness of genotypes and return winner and loser indicies.
- * If fitnesses are equal, then still return one as
- * winner and one as loser to allow for mutation.
+ * SELECCIÓN POR TORNEO
+ * Compara los fitness de los cromosomas y devuelve un arreglo con 2 valores:
+ * [1, 0] si ganó el cromosoma 1 y [0, 1] si ganó el cromosoma 2.
+ * Si los fitness son iguales, retorna igualmente un arreglo con quién ganó y
+ * quién perdió.
  * @param {Genotype} G1
  * @param {Genotype} G2
  */
@@ -56,22 +71,35 @@ tournament = (G1, G2) => {
   }
 };
 
-//CRUZA EN 2 PUNTOS
 /**
- * Split each Genotype by a two random indices and concatenate.
- * @param {Genotype} winner
- * @param {Genotype} loser
+ * CRUZA EN 2 PUNTOS
+ * Se divide a cada cromosoma en 2 puntos aleatorios y luego se los
+ * concatena para generar los 2 hijos.
+ * @param {Genotype} padre1
+ * @param {Genotype} padre2
  */
 twoPointCrossover = (padre1, padre2) => {
   let son1, son2, sections;
   console.log('---CRUZA EN 2 PUNTOS---');
   console.log('Competidores: ' + padre1 + ', ' + padre2);
   let cut = 0;
+  /**
+   * El siguiente DO - WHILE tiene como criterio de corte que ambos hijos
+   * tengan un fitness válido. Como esto puede no cortar nunca, se agregó
+   * además que se permitan hasta 15 iteraciones.
+   */
   do {
+    /**
+     * en "sections" se almacenan las dos posiciones random
+     * en donde se partirán a los cromosomas
+     */
     sections = [
       Math.floor(Math.random() * padre1.length),
       Math.floor(Math.random() * padre1.length),
     ].sort((a, b) => a - b);
+    /**
+     * concatenación para generar ambos hijos
+     */
     son1 = [].concat(
       padre1.slice().splice(0, sections[0]),
       padre2.slice().splice(sections[0], sections[1] - sections[0]),
@@ -87,17 +115,26 @@ twoPointCrossover = (padre1, padre2) => {
   console.log('Hijos resultantes: ' + son1 + ', ' + son2);
   console.log('------');
 
+  /**
+   * Si los hijos generados que se obtuvieron son válidos se devuelve los mismos,
+   * de lo contrario se devuelven los padres, ya que sabemos que ellos tienen
+   * fitness válidos. Esto es debido a que se busca mantener una población que solo
+   * incluya cromosomas válidos.
+   */
   return cut >= 15 ? [padre1, padre2] : [son1, son2];
 };
 
-//MUTACIÓN - REVERTIR
 /**
- * Mutate the supplied Genotype with the supplied genes
+ * MUTACIÓN POR MÉTODO DE REVERTIR
+ * Se muta al cromosoma en cuestión con el alelo contrario al que tiene actualmente
+ * en una posición random del mismo. Que el cromosoma se mute recae en la probabilidad
+ * asignada por probability.
  * @param {Genotype} G
  */
 mutate = (G) => {
   if (Math.random() < probability) {
     do {
+      // se elige una posición random a mutar
       const pos = Math.floor(Math.random() * G.length);
       G[pos] = G[pos] ? 0 : 1;
     } while (fitness(G) > 700);
@@ -105,10 +142,10 @@ mutate = (G) => {
   return G;
 };
 
-// AUXILIARES
+// FUNCIONES AUXILIARES
 
-/*
- * Select two random indices of the population
+/**
+ * Selecciona dos índices random de la población
  */
 selectRandomGenotypes = (tamanioPadres) => {
   let index1, index2;
@@ -119,6 +156,11 @@ selectRandomGenotypes = (tamanioPadres) => {
   return [index1, index2];
 };
 
+/**
+ * Función que evalua si dentro de la población actual existe un cromosoma
+ * cuyo fitness se encuentre dentro del rango deseado. En base a esto se
+ * devuelve un objecto con el cromosoma y su fitness.
+ */
 mejorFitness = (poblacion) => {
   let cromosoma;
   const resultado = poblacion.reduce((max, c) => {
@@ -134,7 +176,7 @@ mejorFitness = (poblacion) => {
 
 // -- INICIO del programa
 generatePopulation(10);
-const maxNumeroDeGeneraciones = 10;
+const maxNumeroDeGeneraciones = 20;
 let nroGeneracion = 0;
 
 do {
@@ -163,12 +205,12 @@ do {
   nroGeneracion += 1;
   console.log('---GENERACIÓN NÚMERO: ' + nroGeneracion + '---');
 } while (
-  nroGeneracion < maxNumeroDeGeneraciones ||
+  nroGeneracion < maxNumeroDeGeneraciones &&
   !mejorFitness(poblacion).resultado
 );
 
 // Resultado Final
 const { cromosoma, resultado } = mejorFitness(poblacion);
-console.log('POBLACIÓN FINAL: ', poblacion)
+console.log('POBLACIÓN FINAL: ', poblacion);
 console.log('GENERACIÓN NUMERO: ' + nroGeneracion);
-console.log('CROMOSOMA GANADOR: ' + cromosoma + ',  FITNESS: ' + resultado);
+console.log('CROMOSOMA GANADOR: ' + cromosoma + '  FITNESS: ' + resultado);
